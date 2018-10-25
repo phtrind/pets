4a91d0290c0e3c3e8f5462cf69ff4f66ab94ec52
Pedro Henrique Trindade <phtrind@hotmail.com>
2018-10-24 21:33:16 -0300

    Correção na tela de login.

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -2,6 +2,6 @@ var app = angular.module('pets', [])
 
 app.controller('loginController', function ($scope, $http, $compile, $sce) {
 
-     $scope.isRegister = true;
+    //  $scope.isRegister = true;
 
 });
