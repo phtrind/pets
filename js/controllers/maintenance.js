e37e543a4a5550a55226a129a3c355ca9fc4c9ba
Pedro Henrique Trindade <phtrind@hotmail.com>
2018-10-28 19:08:41 -0300

    Alteração do Google Analytics para o AngularJS. Inclusão de eventos do GA em todos os botões da tela maintenance.

--- js/controllers/maintenance.js
+++ js/controllers/maintenance.js
@@ -4,12 +4,37 @@ app.controller('maintenanceController', function ($scope, $http, $compile, $sce)
 
     $scope.clickBtnResponderPesquisa = function () {
 
-        ga('send', {
-            hitType: 'event',
-            eventCategory: 'Pesquisa',
-            eventAction: 'Click',
-            eventLabel: 'Responder Pesquisa'
-        });
+        gtag('event',
+            'Click',
+            {
+                'event_category': 'Pesquisa',
+                'event_label': 'Responder Pesquisa',
+                'value': 1
+            });
+
+    }
+
+    $scope.clickBtnFacebook = function () {
+
+        gtag('event',
+            'Click',
+            {
+                'event_category': 'Redes Sociais',
+                'event_label': 'Facebook',
+                'value': 1
+            });
+
+    }
+
+    $scope.clickBtnInstagram = function () {
+
+        gtag('event',
+            'Click',
+            {
+                'event_category': 'Redes Sociais',
+                'event_label': 'Instagram',
+                'value': 1
+            });
 
     }
 
