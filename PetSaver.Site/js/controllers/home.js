cb302cba532a31a145f4493ada85acffae1cd2fb
Cilas Henrique de Oliveira <cilas.henrique@gmail.com>
2018-11-10 10:38:51 -0200

    Alterações reuniao 05/11

--- PetSaver.Site/js/controllers/home.js
+++ PetSaver.Site/js/controllers/home.js
@@ -5,6 +5,19 @@ app.controller('homeController', function ($controller) {
         ctrl.base = $controller('baseController', {});
 
         ctrl.isRegister = false;
+
+        
+    $('#modalSaibaMaisA').on('hidden.bs.modal', function () {
+        ctrl.isRegister = true;
+    });
+
+    $('#modalSaibaMaisB').on('hidden.bs.modal', function () {
+        ctrl.isRegister = true;
+    });
+
+    $('#modalSaibaMaisC').on('hidden.bs.modal', function () {
+        ctrl.isRegister = true;
+    });
     // $scope.changeCollapseImage = function () {
 
     //     if (formFiltroOpen){
