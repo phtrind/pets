767260fed2475b337f8340043cae46d2956048e3
Cilas Henrique de Oliveira <cilas.henrique@gmail.com>
2018-11-19 21:41:59 -0200

    Configuração de relatorios.

--- PetSaver.Site/js/controllers/adocoesRelatorio.js
+++ PetSaver.Site/js/controllers/adocoesRelatorio.js
@@ -1,9 +1,31 @@
-app.controller('adocoesRelatorioController', function ($controller) {
+app.controller('adocoesRelatorioController', function ($controller, $http) {
 
         var ctrl = this;
     
         ctrl.base = $controller('baseController', {});
 
     // $scope.nenhumaAdocao = true;
+
+    ctrl.OnInit = function () {
+
+        // TODO: ALTERAR NOME IdAnuncioAtual e IdUsuario PARA O CORRETO
+        if (!ctrl.base.StringIsEmpty(sessionStorage.getItem('IdAnuncioAtual'))) {
+
+            if (!ctrl.base.StringIsEmpty(sessionStorage.getItem('IdUsuario'))) {
+                request.IdUsuario = sessionStorage.getItem('IdUsuario')
+            }
+
+            $http({
+                method: 'POST',
+                url: ctrl.base.servicePath + 'Page/conta/adocoes_relatorio',
+                data: request
+            }).success(function (response) {
+
+                ctrl.Pet.Nome = response.Nome;
+                ctrl.Pet.RacaEspecie = response.RacaEspecie;
+                ctrl.Pet.Status = response.Status;
+            })
+        }
+    }
 
 });
--- PetSaver.Site/js/controllers/doacoesRelatorio.js
+++ PetSaver.Site/js/controllers/doacoesRelatorio.js
@@ -1,9 +1,36 @@
-app.controller('doacoesRelatorioController', function ($controller) {
+app.controller('doacoesRelatorioController', function ($controller, $http) {
 
         var ctrl = this;
     
         ctrl.base = $controller('baseController', {});
 
-    // $scope.nenhumaDoacao = true;
+    // $scope.nenhumaDoacao = true;,
+
+
+    ctrl.OnInit = function () {
+
+        // TODO: ALTERAR NOME IdAnuncioAtual e IdUsuario PARA O CORRETO
+        if (!ctrl.base.StringIsEmpty(sessionStorage.getItem('IdAnuncioAtual'))) {
+
+            if (!ctrl.base.StringIsEmpty(sessionStorage.getItem('IdUsuario'))) {
+                request.IdUsuario = sessionStorage.getItem('IdUsuario')
+            }
+
+            $http({
+                method: 'POST',
+                url: ctrl.base.servicePath + 'Page/conta/adocoes_relatorio',
+                data: request
+            }).success(function (response) {
+
+                ctrl.Pet.Nome = response.Nome;
+                ctrl.Pet.RacaEspecie = response.RacaEspecie;
+                ctrl.Pet.DataCadastro = response.DataCadastro;
+                ctrl.Pet.Status = response.Status;
+                ctrl.Pet.QtdVisualizacoes = response.QtdVisualizacoes;
+                ctrl.Pet.QtdInteressados = response.QtdInteressados;
+
+            })
+        }
+    }
 
 });
