cd59a065b626d2dfb365a2c0c69f028eb608a7e6
Pedro Henrique Trindade <phtrind@hotmail.com>
2018-12-08 21:56:17 -0200

    Implementação da rolagem na tela inicial.

--- PetSaver.Site/js/controllers/home.js
+++ PetSaver.Site/js/controllers/home.js
@@ -98,3 +98,90 @@
     }
 
 });
+
+//--- DEFINE a reusable animation function: ---//
+function scrollThere(targetElement, speed) {
+    // initiate an animation to a certain page element:
+    $('html, body').stop().animate(
+        { scrollTop: targetElement.offset().top }, // move window so target element is at top of window
+        speed, // speed in milliseconds
+        'swing' // easing
+    ); // end animate
+} // end scrollThere function definition
+
+
+//--- START SCROLL EVENTS ---//
+// detect a mousewheel event (note: relies on jquery mousewheel plugin):
+$(window).on('mousewheel', function (e) {
+
+    // get Y-axis value of each div:
+    var div1y = $('#header').offset().top,
+        div2y = $('#servicos').offset().top,
+        div3y = $('#porque').offset().top,
+        div4y = $('#pets').offset().top,
+        div5y = $('#buscar').offset().top,
+        // get window's current scroll position:
+        lastScrollTop = $(this).scrollTop(),
+        // for getting user's scroll direction:
+        scrollDirection,
+        // for determining the previous and next divs to scroll to, based on lastScrollTop:
+        targetUp,
+        targetDown,
+        // for determining which of targetUp or targetDown to scroll to, based on scrollDirection:
+        targetElement;
+
+    // get scroll direction:
+    if (e.originalEvent.deltaY < 0) {
+        scrollDirection = 'up';
+    } else {
+        scrollDirection = 'down';
+    } // end if
+
+    // prevent default behavior (page scroll):
+    e.preventDefault();
+
+    // condition: determine the previous and next divs to scroll to, based on lastScrollTop:
+    if (lastScrollTop === div1y) {
+        targetUp = $('#header');
+        targetDown = $('#servicos');
+    } else if (lastScrollTop === div2y) {
+        targetUp = $('#header');
+        targetDown = $('#porque');
+    } else if (lastScrollTop === div3y) {
+        targetUp = $('#servicos');
+        targetDown = $('#pets');
+    } else if (lastScrollTop === div4y) {
+        targetUp = $('#porque');
+        targetDown = $('#buscar');
+    } else if (lastScrollTop === div5y) {
+        targetUp = $('#pets');
+        targetDown = $('#buscar');
+    } else if (lastScrollTop < div2y) {
+        targetUp = $('#header');
+        targetDown = $('#servicos');
+    } else if (lastScrollTop < div3y) {
+        targetUp = $('#servicos');
+        targetDown = $('#porque');
+    } else if (lastScrollTop < div4y) {
+        targetUp = $('#porque');
+        targetDown = $('#pets');
+    } else if (lastScrollTop < div5y) {
+        targetUp = $('#pets');
+        targetDown = $('#buscar');
+    } else if (lastScrollTop > div5y) {
+        targetUp = $('#buscar');
+        targetDown = $('#buscar');
+    } // end else if
+
+    // condition: determine which of targetUp or targetDown to scroll to, based on scrollDirection:
+    if (scrollDirection === 'down') {
+        targetElement = targetDown;
+    } else if (scrollDirection === 'up') {
+        targetElement = targetUp;
+    } // end else if
+
+    // scroll smoothly to the target element:
+    scrollThere(targetElement, 400);
+
+}); // end on mousewheel event
+//--- END SCROLL EVENTS ---//
